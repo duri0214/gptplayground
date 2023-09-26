ff759650f4a96610b59ea219fdeaf7c7d15881a5
yoshi <yoshitakaOkada0214@gmail.com>
2023-09-26 22:36:03 +0900

    update

--- chat/tests/domain/valueobject/test_pdfdataloader.py
+++ chat/tests/domain/valueobject/test_pdfdataloader.py
@@ -4,7 +4,13 @@ from chat.domain.valueobject.pdfdataloader import PdfDataloader
 
 
 class TestPdfDataloader(TestCase):
-    def test_This_PDF_has_18_pages(self):
-        dataloader = PdfDataloader('./doj_cloud_act_white_paper_2019_04_10.pdf', ['\n\n \n'])
+    def test_this_pdf_has_pages_en(self):
+        # TODO: PDFは割り方が難しい
+        dataloader = PdfDataloader('./doj_cloud_act_white_paper_2019_04_10.pdf')
         self.assertEqual(18, len(dataloader.data))
+        print(dataloader.data)
+
+    def test_this_pdf_has_pages_jp(self):
+        dataloader = PdfDataloader('./令和4年版少子化社会対策白書全体版（PDF版）.pdf')
+        self.assertEqual(6, len(dataloader.data))
         print(dataloader.data)
