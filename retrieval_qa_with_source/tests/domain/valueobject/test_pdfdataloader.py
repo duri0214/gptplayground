c7e64cf57573f6f7791b7ad5f75aa9dd0fc818fe
yoshi <yoshitakaOkada0214@gmail.com>
2024-05-03 11:25:31 +0900

    fix: formatter

--- retrieval_qa_with_source/tests/domain/valueobject/test_pdfdataloader.py
+++ retrieval_qa_with_source/tests/domain/valueobject/test_pdfdataloader.py
@@ -1,4 +1,3 @@
-import os
 from unittest import TestCase
 
 from retrieval_qa_with_source.domain.valueobject.pdfdataloader import PdfDataloader
@@ -6,13 +5,15 @@ from retrieval_qa_with_source.domain.valueobject.pdfdataloader import PdfDataloa
 
 class TestPdfDataloader(TestCase):
     def test_this_pdf_has_pages_en(self):
-        print(os.getcwd())
-        dataloader = PdfDataloader('./retrieval_qa_with_source/tests/domain/valueobject/doj_cloud_act_white_paper_2019_04_10.pdf')
+        dataloader = PdfDataloader(
+            "./retrieval_qa_with_source/tests/domain/valueobject/doj_cloud_act_white_paper_2019_04_10.pdf"
+        )
         self.assertEqual(18, len(dataloader.pages))
         print(dataloader.data)
 
     def test_this_pdf_has_pages_jp(self):
-        print(os.getcwd())
-        dataloader = PdfDataloader('./retrieval_qa_with_source/tests/domain/valueobject/令和4年版少子化社会対策白書全体版（PDF版）.pdf')
+        dataloader = PdfDataloader(
+            "./retrieval_qa_with_source/tests/domain/valueobject/令和4年版少子化社会対策白書全体版（PDF版）.pdf"
+        )
         self.assertEqual(6, len(dataloader.pages))
         print(dataloader.data)
