9c2879cc8c3606b1c7ae54ef54471533356cb4c1
yoshi <yoshitakaOkada0214@gmail.com>
2024-05-03 12:39:03 +0900

    create app line_qa_with_gpt_and_dall-e

---
+++ line_qa_with_gpt_and_dalle/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LineQaWithGptAndDalleConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "line_qa_with_gpt_and_dalle"
