db61f52d16cc3a19c4b9b4e9fc82b462c4bee4ac
yoshi <yoshitakaOkada0214@gmail.com>
2024-05-03 11:17:19 +0900

    fix: tests

--- retrieval_qa_with_source/tests/domain/valueobject/test_pdfdataloader.py
+++ retrieval_qa_with_source/tests/domain/valueobject/test_pdfdataloader.py
@@ -1,3 +1,4 @@
+import os
 from unittest import TestCase
 
 from retrieval_qa_with_source.domain.valueobject.pdfdataloader import PdfDataloader
@@ -5,11 +6,13 @@ from retrieval_qa_with_source.domain.valueobject.pdfdataloader import PdfDataloa
 
 class TestPdfDataloader(TestCase):
     def test_this_pdf_has_pages_en(self):
-        dataloader = PdfDataloader('./doj_cloud_act_white_paper_2019_04_10.pdf')
+        print(os.getcwd())
+        dataloader = PdfDataloader('./retrieval_qa_with_source/tests/domain/valueobject/doj_cloud_act_white_paper_2019_04_10.pdf')
         self.assertEqual(18, len(dataloader.pages))
         print(dataloader.data)
 
     def test_this_pdf_has_pages_jp(self):
-        dataloader = PdfDataloader('./令和4年版少子化社会対策白書全体版（PDF版）.pdf')
+        print(os.getcwd())
+        dataloader = PdfDataloader('./retrieval_qa_with_source/tests/domain/valueobject/令和4年版少子化社会対策白書全体版（PDF版）.pdf')
         self.assertEqual(6, len(dataloader.pages))
         print(dataloader.data)
