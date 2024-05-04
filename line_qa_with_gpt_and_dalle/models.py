cb63d5ab34f0186cfb1ffc5bf93f98b96a74e1ec
yoshi <yoshitakaOkada0214@gmail.com>
2024-05-04 19:42:42 +0900

    fix: 修正

--- line_qa_with_gpt_and_dalle/models.py
+++ line_qa_with_gpt_and_dalle/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class ChatLogsWithLine(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    thread = models.CharField(max_length=255)
     role = models.CharField(max_length=255)
-    message = models.TextField()
+    content = models.TextField()
     file_path = models.CharField(max_length=255, null=True)
+    invisible = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
