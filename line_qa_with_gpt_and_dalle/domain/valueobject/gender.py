bde84389ea9181f3d359fb5e263f75b7cc4fdc74
yoshi <yoshitakaOkada0214@gmail.com>
2024-11-13 23:01:54 +0900

    Remove unused Django app and related files

--- line_qa_with_gpt_and_dalle/domain/valueobject/gender.py
+++
@@ -1,9 +0,0 @@
-class Gender:
-    def __init__(self, gender):
-        if gender not in {"man", "woman"}:
-            raise ValueError("Invalid gender")
-        self.gender = gender
-
-    @property
-    def name(self) -> str:
-        return "男性" if self.gender == "man" else "女性"
--- line_qa_with_gpt_and_dalle/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = "line_qa_with_gpt"
-urlpatterns = [
-    path("", views.HomeView.as_view(), name="home"),
-    # path("line_webhook/", views.LineWebHookView.as_view(), name="line_webhook"),
-]
